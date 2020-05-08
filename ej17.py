5fc6c3c60dbb31ff39c5b2ff252492faa618c1da
Franco Losardo <francolosa@hotmail.com>
2020-05-08 16:13:26 -0300

    The output has format now

--- ej17.py
+++ ej17.py
@@ -2,8 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 # Here you enter the site's url from where you want to get the titles
 r = requests.get('https://lanacion.com.ar')
+# Converts HTML into string
 r_html = r.text
 soup = BeautifulSoup(r_html, features="html.parser")
-# Here you enter the HTML tag that containes all those titles you want to print
-h1_titles = soup(class_="com-title")
-print(h1_titles)
+# Here you enter the HTML tag that containes the titles you want to print
+# Gives format to the output, the loop removes the spaces and gets rid
+# of all the HTML stuff
+
+for titles in soup(class_="com-title"):
+    if titles.a:
+        print(titles.a.text.replace("\n", "").strip())
+    else:
+        print(titles.contents[0])
