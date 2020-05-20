e7cf60e8d2f7dd577bfa0e30bd5e651bea6b7489
Franco Losardo <francolosa@hotmail.com>
2020-05-20 02:05:09 -0300

    File Overlap

---
+++ ej23.py
@@ -0,0 +1,13 @@
+with open('happyNumbers.txt', 'r') as f, open('primeNumbers.txt', 'r') as f2:
+    happy_numbers = f.read()
+    prime_numbers = f2.read()
+    # Converts file to list
+    formatted_happys = happy_numbers.split()
+    formatted_primes = prime_numbers.split()
+
+    overlapping = []
+    for number in formatted_happys:
+        if number in formatted_primes:
+            overlapping.append(int(number))
+
+print(overlapping)
